cd052254f3ab7ce82ae1632015494a2312630a61
RUzai <RUzairov@gmail.com>
2024-01-15 16:53:28 +0300

    javadoc

--- src/main/java/team/choodoo/orm/sql/Converters.java
+++ src/main/java/team/choodoo/orm/sql/Converters.java
@@ -1,7 +1,6 @@
 package team.choodoo.orm.sql;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import team.choodoo.orm.utils.Constants;
 
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
@@ -18,22 +17,57 @@ public class Converters {
 
     // * WRITE
 
+    /**
+     * Generates LinkedHashMap from bean
+     *
+     * @param bean Serializable
+     * @param <T>  Generic
+     * @return LinkedHashMap
+     */
     public static <T> LinkedHashMap<String, Object> getMap(T bean) {
         return objectMapper.convertValue(bean, LinkedHashMap.class);
     }
 
+    /**
+     * Using if LinkedHashMap contains another LinkedHashMap
+     *
+     * @param map example:
+     *            <code>
+     *            {
+     *            id: 0,
+     *            name: "John Doe",
+     *            position: "SEO"
+     *            }
+     *            </code>
+     * @return example: "0::John Doe::SEO"
+     */
     private static String innerMapToString(LinkedHashMap<String, Object> map) {
         return map.values().stream()
                 .map(Object::toString)
                 .collect(Collectors.joining(FIELDS_DELIMITER));
     }
 
+    /**
+     * Using if LinkedHashMap contains another LinkedHashMap
+     *
+     * @param list example:
+     *             <code>
+     *             ["Jonh Doe", "Sarah Doe"]
+     *             </code>
+     * @return "John Doe--Sarah Doe"
+     */
     private static String innerListToString(ArrayList<LinkedHashMap<String, Object>> list) {
         return list.stream()
                 .map(Converters::innerMapToString)
                 .collect(Collectors.joining(BEANS_DELIMITER));
     }
 
+    /**
+     * Converts object to SQL insert string
+     *
+     * @param value example: 0
+     * @return example: "'0'"
+     */
     private static String toValues(Object value) {
         String valueString = value.toString();
         if (value instanceof LinkedHashMap valueMap) valueString = innerMapToString(valueMap);
@@ -41,6 +75,12 @@ public class Converters {
         return String.format(SQL_VALUE_WRAPPER, valueString);
     }
 
+    /**
+     * Converts object to SQL update string
+     *
+     * @param entry example: {id: 0}
+     * @return example: "id = '0'"
+     */
     private static String toKeyValues(Map.Entry<String, Object> entry) {
         Object value = entry.getValue();
         String valueString = value.toString();
@@ -49,12 +89,39 @@ public class Converters {
         return String.format(SQL_KEY_VALUE_WRAPPER, getColumnName(entry.getKey()), valueString);
     }
 
+    /**
+     * Converts LinkedHashMap to SQL insert string
+     *
+     * @param map example:
+     *            <code>
+     *            {
+     *            id: 0,
+     *            name: "John Doe",
+     *            position: "SEO"
+     *            }
+     *            </code>
+     * @return example: "'0', 'John Doe', 'SEO'"
+     */
     public static String mapToValues(LinkedHashMap<String, Object> map) {
         return map.values().stream()
                 .map(Converters::toValues)
                 .collect(Collectors.joining(SQL_COMMA));
     }
 
+
+    /**
+     * Converts LinkedHashMap to SQL update string
+     *
+     * @param map example:
+     *            <code>
+     *            {
+     *            id: 0,
+     *            name: "John Doe",
+     *            position: "SEO"
+     *            }
+     *            </code>
+     * @return example: "id = '0', name = 'John Doe', position = 'SEO'"
+     */
     public static String mapToKeyValues(LinkedHashMap<String, Object> map) {
         return map.entrySet().stream()
                 .map(Converters::toKeyValues)
@@ -63,6 +130,9 @@ public class Converters {
 
     // * READ
 
+    /**
+     * Read data from ResultSet
+     */
     public static <T> List<T> readData(Class<T> type, ResultSet rs) throws SQLException {
         List<T> list = new ArrayList<>();
         while (rs.next()) {
@@ -71,6 +141,9 @@ public class Converters {
         return list;
     }
 
+    /**
+     * Read object from ResultSet
+     */
     private static <T> T readObject(Class<T> type, ResultSet rs) throws SQLException {
         ResultSetMetaData md = rs.getMetaData();
         int columnCount = md.getColumnCount();
